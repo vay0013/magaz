481bf3da69d603da743029592e3062cee372583f
vay0013 <prop615111@gmail.com>
2023-07-22 19:16:17 +1000

    first

---
+++ src/main/java/com/vay/magaz/MagazApplication.java
@@ -0,0 +1,13 @@
+package com.vay.magaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagazApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vay/magaz/MagazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vay.magaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
